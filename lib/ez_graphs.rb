b7ece27ec26d84da518091fdbeb84c6c156a6bdf
Maksim Kremez <mr.kremez@gmail.com>
2023-02-03 17:32:41 +0300

    Init gem structure

---
+++ lib/ez_graphs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ez_graphs/version"
+
+module EzGraphs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ez_graphs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EzGraphs do
+  it "has a version number" do
+    expect(EzGraphs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
