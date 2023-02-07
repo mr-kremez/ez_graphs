a61b4aa6c8d23947464ce1c33bf689945b8cf2f4
Maksim Kremez <mr.kremez@gmail.com>
2023-02-07 15:59:14 +0300

    Fix specs

--- spec/ez_graphs_spec.rb
+++ spec/ez_graphs_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe EzGraphs do
   it "has a version number" do
     expect(EzGraphs::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
